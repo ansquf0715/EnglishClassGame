f56a8f1a28ee95b4083510dc39aeabc0cf2ea278
[ansquf0715] <ansquf0715@naver.com>
2024-08-23 00:37:52 +0900

    설정 페이지 구현

--- Assets/Setting.cs
+++ Assets/Setting.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Setting : MonoBehaviour
 {
+    bool settingOn = false;
+    Image SettingPage;
+
+    AudioSource bgmAudio;
+    AudioSource effectAudio;
+
+    Slider bgmSlider;
+    Slider effectSlider;
+
     // Start is called before the first frame update
     void Start()
     {
+        Canvas canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+
+        SettingPage = canvas.transform.Find("SettingPage").GetComponent<Image>();
         
+        bgmAudio = GameObject.Find("Audio Source").GetComponent<AudioSource>();
+        effectAudio = GameObject.Find("Effect Audio Source").GetComponent<AudioSource>();
+        
+        bgmSlider = SettingPage.transform.Find("BGMSlider").GetComponent<Slider>();
+        bgmSlider.value = bgmAudio.volume;
+        bgmSlider.onValueChanged.AddListener(MusicSoundSlider);
+
+        effectSlider = SettingPage.transform.Find("EffectSlider").GetComponent<Slider>();
+        effectSlider.value = effectAudio.volume;
+        effectSlider.onValueChanged.AddListener(EffectSoundSlider);
     }
 
     // Update is called once per frame
@@ -18,6 +42,46 @@ public class Setting : MonoBehaviour
 
     public void SettingButtonClicked()
     {
+        //settingOn = SettingPage.gameObject.activeSelf;
+        SettingPage.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
 
+    public void SettingPageExitClicked()
+    {
+        SettingPage.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MusicSoundSlider(float volume)
+    {
+        bgmAudio.volume = volume;
+    }
+
+    public void EffectSoundSlider(float volume)
+    {
+        effectAudio.volume = volume;
+    }
+
+    public void restartButtonClicked()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void settingFinishClicked()
+    {
+        Time.timeScale = 1;
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void TeacherButtonClicked()
+    {
+        Time.timeScale = 1;
+        Debug.Log("Teacher Button Clicked");
     }
 }
